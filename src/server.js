039eb72903dbdf30ccf54eb615313a32e4a4995b
Ronaldo jose <ronaldo.jrk@gmail.com>
2020-05-11 19:02:07 -0300

    configurando port

--- src/server.js
+++ src/server.js
@@ -9,6 +9,4 @@ app.use(routes);
 
 //app.listen(3000);
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(process.env.PORT || 3000);

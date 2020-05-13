b0274a8b06e972ca6841c422ade131ebc80a1789
Ronaldo jose <ronaldo.jrk@gmail.com>
2020-05-13 11:27:53 -0300

    corrigindo erros da troca de base

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@
 require('dotenv/config');
 
 
-console.log(process.env.db_DIALECT,process.env.db_HOST,process.env.db_USERNAME,process.env.db_PASSWORD,process.env.db_DATABASE)
+//console.log(process.env.db_DIALECT,process.env.db_HOST,process.env.db_USERNAME,process.env.db_PASSWORD,process.env.db_DATABASE)
 module.exports ={
   dialect:process.env.db_DIALECT,
   host :process.env.db_HOST,

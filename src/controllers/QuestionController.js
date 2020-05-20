494dd117ee0ba65a842f042def25f7e4a333a42b
Ronaldo jose <ronaldo.jrk@gmail.com>
2020-05-20 15:49:12 -0300

    add question

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,104 +1,116 @@
 
 const Question = require('../models/Question');
-const User= require('../models/User');
+const User = require('../models/User');
 
-  module.exports = {
-    //cadastrar uma question
-    async store(req, res){
+module.exports = {
+  //cadastrar uma question
+  async store(req, res) {
 
-      try {
-              const {user_id} = req.params;
-            const {enunciated,alternativetrue,alternativea,alternativeb,alternativec,timelimite}=req.body;
+    try {
+      const { user_id } = req.params;
+      const { enunciated, alternativetrue, alternativea, alternativeb, alternativec, timelimite } = req.body;
 
-            const user = await User.findByPk(user_id);
+      const user = await User.findByPk(user_id);
 
-            if(!user){
-              return res.status(400).json({message:'User not found'});
+      if (!user) {
+        return res.status(400).json({ message: 'User not found' });
 
-            }
-            const question = await Question.create({
-              enunciated,
-              alternativetrue,
-              alternativea,
-              alternativeb,
-              alternativec,
-              timelimite,
-              user_id,
-            });
-
-            return res.json(question);
-        
-      } catch (error) {
-          console.log("deu erro");
-        console.log(error);
-        
       }
 
-    },
+      const record = await Question.count({
+        where: {
+          enunciated: enunciated,
+        }
+
+      });
+
+      if (record != 0) {
+        return res.status(400).json({ message: 'ja existe questão com esse enuciado' });
+
+      }
+      const question = await Question.create({
+        enunciated,
+        alternativetrue,
+        alternativea,
+        alternativeb,
+        alternativec,
+        timelimite,
+        user_id,
+      });
+
+      return res.json(question);
+
+    } catch (error) {
+      console.log("deu erro");
+      console.log(error);
+
+    }
+
+  },
 
 
-    // pegando todas as question daquele usuario
-    async index(req, res){
-    
-      try {
-            const {user_id} = req.params;
+  // pegando todas as question daquele usuario
+  async index(req, res) {
 
-          const user = await User.findByPk(user_id,{
-            include:{ association: 'questions' }
-          });
+    try {
+      const { user_id } = req.params;
 
-          if(!user){
-            return res.status(400).json({message:'User not found'});
+      const user = await User.findByPk(user_id, {
+        include: { association: 'questions' }
+      });
 
-          }
+      if (!user) {
+        return res.status(400).json({ message: 'User not found' });
 
-          return res.json(user);
-        
-      } catch (error) {
-
-        console.log("deu erro");
-        console.log(error);
-        
       }
 
+      return res.json(user);
+
+    } catch (error) {
+
+      console.log("deu erro");
+      console.log(error);
+
+    }
 
 
 
 
-    },
-    // pegando todas as question daquele usuario
-    async indexquestion(req, res){
-    
-      try {
+
+  },
+  // pegando todas as question daquele usuario
+  async indexquestion(req, res) {
+
+    try {
 
 
-              const {user_id} = req.params;
+      const { user_id } = req.params;
 
-            const user = await User.findByPk(user_id,{
-              include:{ association: 'questions' }
-            });
+      const user = await User.findByPk(user_id, {
+        include: { association: 'questions' }
+      });
 
-            if(!user){
-              return res.status(400).json({message:'User not found'});
+      if (!user) {
+        return res.status(400).json({ message: 'User not found' });
 
-            }
-
-            return res.json(user.questions);
-        
-      } catch (error) {
-
-
-        console.log("deu erro");
-        console.log(error);
-
-        
       }
 
+      return res.json(user.questions);
+
+    } catch (error) {
 
 
+      console.log("deu erro");
+      console.log(error);
 
 
     }
 
 
-  };
+
+
+
+  }
+
+
+};
